ff7b8e0d5e2f097adf4e122df143b324eb97ecfc
Duncan Sterken <duncte123@users.noreply.github.com>
2017-10-11 10:23:32 +0200

    Add creator info to footer

--- pages/templates/footer.php
+++ pages/templates/footer.php
@@ -1,4 +1,13 @@
-
+  <footer class="text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <hr />
+          <p>Created by <a href="http://duncte123.ml/" target="_blank">duncte123</a>.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
