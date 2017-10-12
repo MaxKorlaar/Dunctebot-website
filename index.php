737cdb3fe114650bc97d159bf014b2ec6943ed20
Max Korlaar <max@maxkorlaar.nl>
2017-10-12 14:11:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-$page = empty($_GET['page']) ? "home" : $_GET['page'];
+$page = empty($_GET['page']) ? "home" : filter_input(INPUT_GET, 'page', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 require("pages/templates/header.php");
 
-if(file_exists("pages/{$page}.php")) {
+$allowedPages = ['home', 'kpop', 'quotes'];
+
+if(in_array($page, $allowedPages)) {
     require("pages/{$page}.php");
 } else {
     require('pages/404.php');
